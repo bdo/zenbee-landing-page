2ba4d53e7eb2b173bd2e5e8415404284dfa2e44a
Benoit d'Oncieu <bdoncieu@gmail.com>
2018-01-22 20:28:58 +0100

    add pictures

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Button from 'material-ui/Button'
-import Typography from 'material-ui/Typography'
+import { Button, Typography } from 'material-ui'
 import { withStyles } from 'material-ui/styles'
 import withRoot from '../../withRoot'
-import logo from '../../theme/images/zenbee-logo.png'
-import beeLeft from '../../theme/images/bee-left.png'
-import beeRight from '../../theme/images/bee-right.png'
-import bee from '../../theme/images/bee.png'
+import logo from './images/zenbee-logo.png'
+import beeLeft from './images/bee-left.png'
+import beeRight from './images/bee-right.png'
+import bee from './images/bee.png'
+import picture1 from './images/picture-1.jpg'
+import picture2 from './images/picture-2.jpg'
+import picture3 from './images/picture-3.jpg'
+import picture4 from './images/picture-4.jpg'
 
 const styles = theme => ({
   root: {
@@ -57,6 +60,43 @@ const styles = theme => ({
       display: 'none',
     },
   },
+  pictures: {
+    padding: 0,
+    margin: '100px -5%',
+    display: 'flex',
+    justifyContent: 'space-around',
+  },
+  picture: {
+    flex: '1 0 auto',
+    height: 'auto',
+    marginLeft: '2%',
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+    borderRadius: 4,
+    boxShadow: '0 10px 20px 0 rgba(0, 0, 0, 0.15)',
+    content: '""',
+    '&:before': {
+      content: '""',
+      float: 'left',
+      paddingTop: '100%',
+    },
+  },
+  picture1: {
+    backgroundImage: `url(${picture1})`,
+    marginLeft: 0,
+  },
+  picture2: {
+    transform: 'translateY(30px)',
+    backgroundImage: `url(${picture2})`,
+  },
+  picture3: {
+    transform: 'translateY(14px)',
+    backgroundImage: `url(${picture3})`,
+  },
+  picture4: {
+    transform: 'translateY(46px)',
+    backgroundImage: `url(${picture4})`,
+  },
 })
 
 const HomePage = ({ classes }) =>
@@ -83,6 +123,12 @@ const HomePage = ({ classes }) =>
           Start your trip
         </Button>
       </div>
+    </div>
+    <div className={classes.pictures}>
+      <div className={`${classes.picture} ${classes.picture1}`} />
+      <div className={`${classes.picture} ${classes.picture2}`} />
+      <div className={`${classes.picture} ${classes.picture3}`} />
+      <div className={`${classes.picture} ${classes.picture4}`} />
     </div>
   </div>
 
